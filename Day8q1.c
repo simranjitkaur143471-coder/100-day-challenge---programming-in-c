f122f74202aa5cb1d4046fee1bb58ed3f0045b26
simranjitkaur143471-coder <simranjitkaur143471@gmail.com>
2025-11-06 22:55:44 +0530

    Day8q1 and q2

---
+++ Day8q1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    float num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &operator); 
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2f\n", result);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Result: %.2f\n", num1 / num2);
+            else
+                printf("Error! Division by zero is not allowed.\n");
+            break;
+
+        case '%':
+        
+            if ((int)num2 != 0)
+                printf("Result: %d\n", (int)num1 % (int)num2);
+            else
+                printf("Error! Modulo by zero is not allowed.\n");
+            break;
+
+        default:
+            printf("Invalid operator! Please use +, -, *, /, or %%.\n");
+    }
+
+    return 0;
+}
